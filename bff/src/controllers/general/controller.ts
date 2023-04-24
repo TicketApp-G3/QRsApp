f5aebc38f31e4bda142958f6e128d67ce9269eff
Ariel0101 <32275135+Ariel0101@users.noreply.github.com>
2023-04-24 18:39:15 -0300

    Add status filter to own events (#6)
    * Add status filter
    
    * Correct BASE URL

--- bff/src/controllers/general/controller.ts
+++ bff/src/controllers/general/controller.ts
@@ -13,7 +13,12 @@ class GeneralController {
   }
 
   public async getOwnerEvents(req: Request): Promise<any[]> {
-    const response = await axios.get(`${BASE_URL}/events`, { params: { ownerId: req.params.userId } })
+    const response = await axios.get(`${BASE_URL}/events`, {
+      params: {
+        ownerId: req.params.userId,
+        status: "IN_PROGRESS",
+      }
+    })
     return response.data
   }
 
