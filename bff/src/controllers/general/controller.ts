abc971f9482f465667d5e291bded989abae4ea5c
Fer Alvarez <fgalvarez@fi.uba.ar>
2023-05-11 20:15:44 -0300

    fix docker

--- bff/src/controllers/general/controller.ts
+++ bff/src/controllers/general/controller.ts
@@ -5,7 +5,7 @@ import { Request, UserPlatforms } from '@shared';
 const EVENTS_BASE_URL = "https://ticket-app-ms-events.onrender.com" // PROD
 // const EVENTS_BASE_URL = 'http://event_ms:8080'  // LOCAL
 
-const USERS_BASE_URL = 'http://event_ms:8080'  // LOCAL
+const USERS_BASE_URL = 'http://event_ms:8084'  // LOCAL
 
 class GeneralController {
   private logger;
